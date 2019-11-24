9e8e9caadc953c810392ed3b5a0f4987947bf9c6
alecsutzuu <alecsutzuu@gmail.com>
2019-11-24 04:48:57 +0200

    Initial commit

---
+++ src/inmultirea.java
@@ -0,0 +1,11 @@
+public class inmultirea {
+    public static void main(String[] args) {
+        int col, lin;
+        for (lin = 1; lin <= 9; lin++) {
+            for (col = 1; col <= 9; col++)
+                System.out.println(col + "x" + lin + "=" + col * lin);
+            Object muie;
+            muie
+        }
+    }
+}
